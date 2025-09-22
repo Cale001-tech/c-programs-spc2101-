460fe4d51965acf33d26853ac6b4b517729b90ce
Caleb26457 <rockiecalebz@gmail.com>
2025-09-22 11:43:50 +0300

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,18 @@
+/*
+autor:Calebweru
+reg no:26457
+date:21st sept 2025
+description:program to add two numbers
+*/
+#include<stdio.h> //scanf(), printf()
+
+int main() {
+  //variable declaration &initialization
+	int num1=10;
+	int num2 =20;
+	int sum;
+	sum = num1 +num2;
+	printf("the sum is %d ", sum);
+	
+	return 0;
+}
