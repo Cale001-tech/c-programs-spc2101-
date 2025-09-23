e8a6e8d3cf7314eefe1fa7e983f4106d39b91262
Caleb26457 <rockiecalebz@gmail.com>
2025-09-23 12:28:47 +0300

    Add files via upload

---
+++ loan assignment programming.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int age, income;
+	printf("enter users age: \n");
+	scanf("%d" , &age);
+	printf("enter users annual income: \n");
+	scanf("%d" , &income);
+	if(age>=20 && income>=18000) { 
+		printf("congratulations you qualify for a loan. \n");
+	} else {
+		printf("unfortunately we are unable to offer you a loan at this time, \n");
+	}
+	return 0;
+	}
