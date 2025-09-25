69e43b6ef0281a25ffe4115f6c5c8823fae4c941
Caleb26457 <rockiecalebz@gmail.com>
2025-09-25 22:37:30 +0300

    Add files via upload

---
+++ programming to display computer units semester 1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // Semester 1 Computer Science Units
+    char *units[] = {
+        "1. Introduction to Computer Science",
+        "2. Computer Organization and Architecture",
+        "3. Introduction to Programming in C",
+        "4. Discrete Mathematics",
+        "5. Communication Skills",
+        "6. Fundamentals of Information Systems"
+    };
+    
+    int n = sizeof(units) / sizeof(units[0]);  // Number of units
+
+    printf("=== Computer Science Semester 1 Units ===\n\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", units[i]);
+    }
+
+    return 0;
+}
