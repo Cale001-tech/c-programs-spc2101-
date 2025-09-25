ff18c5ca1f1e7e7f09ae978dc1276233ebd3ef70
Caleb26457 <rockiecalebz@gmail.com>
2025-09-25 22:47:22 +0300

    Update programming to display computer units semester 1.c

--- programming to display computer units semester 1.c
+++ programming to display computer units semester 1.c
@@ -1,14 +1,20 @@
+/*
+    Name:Caleb Weru
+Reg no:26457
+    */
 #include <stdio.h>
 
 int main() {
     // Semester 1 Computer Science Units
     char *units[] = {
-        "1. Introduction to Computer Science",
-        "2. Computer Organization and Architecture",
-        "3. Introduction to Programming in C",
-        "4. Discrete Mathematics",
-        "5. Communication Skills",
-        "6. Fundamentals of Information Systems"
+    "1. Introduction to Computer Science",
+    "2. Computer Organization and Architecture",
+    "3. Introduction to Programming in C",
+    "4. Discrete Mathematics",
+    "5. Communication Skills",
+    "6. Fundamentals of Information Systems",
+    "7.mathematics for science",
+    "8.physics",
     };
     
     int n = sizeof(units) / sizeof(units[0]);  // Number of units
@@ -21,3 +27,4 @@ int main() {
 
     return 0;
 }
+
